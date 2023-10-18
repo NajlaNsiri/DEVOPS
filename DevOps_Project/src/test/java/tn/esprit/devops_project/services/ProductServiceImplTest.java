878d8f1da57ce33a36167fd45e2ebf6251e13b63
NajlaNsiri <najla.nsiri@esprit.tn>
2023-10-18 10:08:56 -0400

    initial commit

---
+++ DevOps_Project/src/test/java/tn/esprit/devops_project/services/ProductServiceImplTest.java
@@ -0,0 +1,21 @@
+package tn.esprit.devops_project.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class ProductServiceImplTest {
+
+    @Test
+    void addProduct() {
+    }
+
+    @Test
+    void retrieveProduct() {
+    }
+
+    @Test
+    void deleteProduct() {
+    }
+}
