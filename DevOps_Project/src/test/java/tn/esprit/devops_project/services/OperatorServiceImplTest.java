b494bc803b5ec4ac349d879f6f93884c03519380
Nour Ben Mehrez <nour.benmehrez@esprit.tn>
2023-10-18 15:04:07 +0100

    initial commit

---
+++ DevOps_Project/src/test/java/tn/esprit/devops_project/services/OperatorServiceImplTest.java
@@ -0,0 +1,21 @@
+package tn.esprit.devops_project.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class OperatorServiceImplTest {
+
+    @Test
+    void addOperator() {
+    }
+
+    @Test
+    void deleteOperator() {
+    }
+
+    @Test
+    void retrieveOperator() {
+    }
+}
